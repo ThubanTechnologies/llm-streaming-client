2230b2a23e9979853c1df4349c7a3fc3f6ea9ab6
Jesús Alessandro Ponce Farfán <jponce@thubantech.com>
2025-06-17 19:47:26 +0200

    [UPDATE] Update setup for installable versions

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='llm_streaming-client',
-    version='1.0.0',
-    author='Alba Marco Ugarte',
-    author_email='amarco@thubantech.com',
-    description='A simplified client for llm-streaming services.',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="llm_streaming-client",
+    version="1.0.0",
+    author="Alba Marco Ugarte",
+    author_email="amarco@thubantech.com",
+    description="A simplified client for llm-streaming services.",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-        'setuptools',
-        'requests==2.32.3',
-        'python-dotenv==1.0.1',
-        'python-socketio==5.12.1'
-
+        "setuptools",
+        "requests>=2.32.3",
+        "python-dotenv>=1.1.0",
+        "python-socketio==5.12.1",
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
-    license='MIT',
-    python_requires='>=3.9',
+    license="MIT",
+    python_requires=">=3.9",
 )
